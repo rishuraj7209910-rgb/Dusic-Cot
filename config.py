b086e2e8af9473be0b4724a44691b5794c438225
rishuraj7209910-rgb <rishu.raj7209910@gmail.com>
2026-04-20 00:40:48 +0530

    Fix syntax error in START_IMG_URL assignment

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ STRING5 = os.getenv("STRING_SESSION5", None)
 
 AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 
-START_IMG_URL = "https://files.catbox.moe/7q8bfg.jpg")
+START_IMG_URL = "https://files.catbox.moe/7q8bfg.jpg"
 PING_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
 PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
